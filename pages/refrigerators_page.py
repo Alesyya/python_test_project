fd74008c4f1bd5b134010560c39ea720ac8fc42a
Lukashevich Alesya <Lukasheviich03@gmail.com>
2023-11-14 17:21:51 +0300

    add more tests

--- pages/refrigerators_page.py
+++ pages/refrigerators_page.py
@@ -6,9 +6,17 @@ from helpers import BasePage
 
 
 class RefrigeratorsPage(BasePage):
+    APPLY_BUTTON_LOCATOR = "//div[text()='Применить фильтр']"
     text_refrigerators_locator = "//h1[@class='style_title__1u7Nj']"
     PRICE_MIN_LOCATOR = "//input[@id='minPrice']"
     PRICE_MAX_LOCATOR = "//input[@id='maxPrice']"
+    ATLANT_CHECKBOX_LOCATOR = "//button[@data-testid='producer-atlant']//div[@class='styles_checkbox__18jKd']"
+    product_text_locator = "//p[@class='CardInfo_info__2XVo8 style_fullNameContainer__3kd0q']"
+    ONE_CHAMBER_BUTTON_LOCATOR = "//a[text()='Однокамерные']"
+    text_title_one_chamber_locator = "//h1[text()='Холодильники однокамерные']"
+    TWO_CHAMBER_LOCATOR = "//a[text()='Двухкамерные']"
+    text_title_two_chamber_locator = "//h1[text()='Холодильники двухкамерные']"
+    SORT_RESET_BUTTON_LOCATOR = "//div[@class='style_applyButtonWrapper__1esCD']//span"
 
     @allure.step("Assert text in button and title refrigerators")
     def assert_text_in_button_and_title_of_page_refrigerators(self):
@@ -33,3 +41,39 @@ class RefrigeratorsPage(BasePage):
         self.wait_for_visible(self.PRICE_MIN_LOCATOR)
         price_in_button = self.driver.find_element(By.XPATH, self.PRICE_MIN_LOCATOR).get_attribute("value")
         assert price_in_button == "83,72", "Automatic playback of minimum price did not work"
+
+    @allure.step("Click on atlant checkbox")
+    def click_on_atlant_checkbox(self):
+        self.scroll_to_element(self.ATLANT_CHECKBOX_LOCATOR)
+        self.click_on(self.ATLANT_CHECKBOX_LOCATOR)
+
+    @allure.step("Click on apply button")
+    def click_on_apply_button(self):
+        self.click_on(self.APPLY_BUTTON_LOCATOR)
+
+    @allure.step("Assert automatic correction of incorrect min price")
+    def assert_sorting_manufacturer(self):
+        text_in_button = self.driver.find_element(By.XPATH, self.product_text_locator).text
+        assert "ATLANT" in text_in_button
+
+    @allure.step("Click on one chamber button")
+    def click_on_one_chamber_button(self):
+        self.click_on(self.ONE_CHAMBER_BUTTON_LOCATOR)
+
+    @allure.step("Assert text in one chamber title")
+    def assert_text_in_one_chamber_title(self):
+        text_in_button = self.driver.find_element(By.XPATH, self.text_title_one_chamber_locator).text
+        assert "однокамерные" in text_in_button
+
+    @allure.step("Click on two chamber button")
+    def click_on_two_chamber_button(self):
+        self.click_on(self.TWO_CHAMBER_LOCATOR)
+
+    @allure.step("Assert text in two chamber title")
+    def assert_text_in_two_chamber_title(self):
+        text_in_button = self.driver.find_element(By.XPATH, self.text_title_two_chamber_locator).text
+        assert "двухкамерные" in text_in_button
+
+    @allure.step("Assert displayed of sort reset")
+    def assert_displayed_of_sort_reset(self):
+        self.assert_element_is_displayed(self.SORT_RESET_BUTTON_LOCATOR)
--- tests/test_refrigerators_page.py
+++ tests/test_refrigerators_page.py
@@ -27,3 +27,52 @@ def test_check_min_price(driver, accept_cookies):
     tires_page.input_uncorrect_min_price()
     tires_page.click_on_max_price()
     tires_page.assert_automatic_correction_of_incorrect_min_price()
+
+
+@allure.feature("Refrigerators")
+@allure.story("Check choose manufacturer in sorting settings")
+@allure.severity('normal')
+def test_choose_manufacturer(driver, accept_cookies):
+    header_element = HeaderElement(driver)
+    header_element.click_on_refrigerators_locator()
+
+    refrigerators_page = RefrigeratorsPage(driver)
+    refrigerators_page.click_on_atlant_checkbox()
+    refrigerators_page.click_on_apply_button()
+    refrigerators_page.assert_sorting_manufacturer()
+
+
+@allure.feature("Refrigerators")
+@allure.story("Check correct work one chamber button")
+@allure.severity('normal')
+def test_one_chamber_button(driver, accept_cookies):
+    header_element = HeaderElement(driver)
+    header_element.click_on_refrigerators_locator()
+
+    refrigerators_page = RefrigeratorsPage(driver)
+    refrigerators_page.click_on_one_chamber_button()
+    refrigerators_page.assert_text_in_one_chamber_title()
+
+
+@allure.feature("Refrigerators")
+@allure.story("Check correct work two chamber button")
+@allure.severity('normal')
+def test_two_chamber_button(driver, accept_cookies):
+    header_element = HeaderElement(driver)
+    header_element.click_on_refrigerators_locator()
+
+    refrigerators_page = RefrigeratorsPage(driver)
+    refrigerators_page.click_on_two_chamber_button()
+    refrigerators_page.assert_text_in_two_chamber_title()
+
+
+@allure.feature("Sort reset")
+@allure.story("Changing sorting settings on the refrigerators page")
+@allure.severity('normal')
+def test_sort_reset_button(driver, accept_cookies):
+    header_element = HeaderElement(driver)
+    header_element.click_on_refrigerators_locator()
+
+    refrigerators_page = RefrigeratorsPage(driver)
+    refrigerators_page.click_on_atlant_checkbox()
+    refrigerators_page.assert_displayed_of_sort_reset()
